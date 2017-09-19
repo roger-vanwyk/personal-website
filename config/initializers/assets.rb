e2885656204589f633a0ba4af75f16864b5777f5
Eric <twinpaireric@gmail.com>
2017-09-18 17:03:08 -0700

    Remove precompiling assets and include them in respected application.css and application.js files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,23 +9,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-
-Rails.application.config.assets.precompile += %w( gonzaelectric.png )
-Rails.application.config.assets.precompile += %w( profile.jpg )
-Rails.application.config.assets.precompile += %w( lego.jpg )
-Rails.application.config.assets.precompile += %w( videdu.png )
-Rails.application.config.assets.precompile += %w( blogger.png )
-Rails.application.config.assets.precompile += %w( lazarus.jpg )
-Rails.application.config.assets.precompile += %w( LosAngeles.jpg )
-Rails.application.config.assets.precompile += %w( imaguana.png )
-Rails.application.config.assets.precompile += %w( sketch.png )
-Rails.application.config.assets.precompile += %w( tank.png )
-Rails.application.config.assets.precompile += %w( acquantion.png )
-Rails.application.config.assets.precompile += %w( eventor.png )
-Rails.application.config.assets.precompile += %w( main.css )
-Rails.application.config.assets.precompile += %w( main.js )
-Rails.application.config.assets.precompile += %w( font-awesome.min.css )
-Rails.application.config.assets.precompile += %w( smoothscroll.js )
-Rails.application.config.assets.precompile += %w( favicon.ico )
-Rails.application.config.assets.precompile += %w( bootstrap.min.css )
-Rails.application.config.assets.precompile += %w( projects.css )
