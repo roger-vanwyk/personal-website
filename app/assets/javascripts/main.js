a8ca12de09fb0d0354afb4ae3f7a9595a76f0c3c
Eric <twinpaireric@gmail.com>
2017-10-05 16:49:35 -0700

    Change smoothscroll.js to a few lines of code. Update 'about me' text

--- app/assets/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-(function(){
-	// Menu settings
-	$('#menuToggle, .menu-close, .nav-link').on('click', function(){
-		$('#menuToggle').toggleClass('active');
-		$('#theMenu').toggleClass('menu-open');
-	});
-})(jQuery)
-
