6cb1e44d1a86f21e082cd60cf2e03fbcd623b2b9
Eric <twinpaireric@gmail.com>
2018-03-19 20:39:16 -0700

    Update terminal txt quote

--- app/assets/javascripts/terminal_shell.js
+++ app/assets/javascripts/terminal_shell.js
@@ -1,7 +1,7 @@
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
-  var txt = `I'm adept with a majority of web development frameworks such as Ruby on Rails and Laravel. I also know my way around front-end JS frameworks such as React and AngularJS. I have experience working with databases (MySQL, PostgreSQL, SQLite), and industry tools (Git, AWS, Slack, Travis-Cl, Heroku).`;
+  var txt = "I'm adept with a majority of web development frameworks such as Ruby on Rails and Laravel. I also know my way around front-end JS frameworks such as React and AngularJS. I have experience working with databases (MySQL, PostgreSQL, SQLite), and industry tools (Git, AWS, Slack, Travis-Cl, Heroku).";
   var speed = 25;
 
   function typeItOut () {
